7b8151daf25255e41f426f72490b79dff762e626
hzee97 <janghyeonji0830@naver.com>
2024-04-12 15:22:06 +0900

    LV2.H-Index

---
+++ LV2/H-Index.py
@@ -0,0 +1,11 @@
+def solution(citations):
+    answer = 0
+    
+    citations.sort(reverse=True)
+    
+    # enumerate : (인덱스,값) 반환
+    for idx,val in enumerate(citations):
+        if idx+1<=val:
+            answer=idx+1
+            
+    return answer
