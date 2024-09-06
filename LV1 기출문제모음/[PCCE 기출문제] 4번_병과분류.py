f6e88f64fc5321eab0327dafaf2edb71854e2367
hzee97 <janghyeonji0830@naver.com>
2024-09-07 05:22:23 +0900

    LV1. [PCCE 기출문제] 4번_병과분류

---
+++ LV1 기출문제모음/[PCCE 기출문제] 4번_병과분류.py
@@ -0,0 +1,15 @@
+# [PCCE 기출문제] 4번_병과분류
+
+code = input()
+last_four_words = code[-4:]
+
+if last_four_words == '_eye':
+    print("Ophthalmologyc")
+elif last_four_words == 'head':
+    print("Neurosurgery")
+elif last_four_words == 'infl':
+    print("Orthopedics")
+elif last_four_words == 'skin':
+    print("Dermatology")
+else:
+    print("direct recommendation")
